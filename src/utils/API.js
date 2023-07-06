3a5ce7dd720c232c2d9e478012c3b7146cb18996
Crivella <davidecrivella@yahoo.it>
2023-07-06 15:27:41 +0200

    Added capability to perform lazy request before sending entire content

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-var ENDPOINT = 'http://127.0.0.1:4000';
+var ENDPOINT;
 
 // axios.defaults.xsrfHeaderName = 'X-CSRFToken'
 // axios.defaults.xsrfCookieName = 'csrftoken'
@@ -10,14 +10,33 @@ export function setEndpoint(endpoint) {
     ENDPOINT = endpoint;
 }
 
-export async function getOcr(md5Hash, base64data) {
-    console.log('GET OCR');
+async function getOcrLazy(md5Hash) {
+    console.log('GET OCR - Lazy');
     const res = await axios.post(`${ENDPOINT}/test/`, {
         md5: md5Hash,
-        contents: base64data,
-    }, {
-        // headers: headers,
     })
 
     return res.data;
+}
+
+async function getOcrWork(md5Hash, base64data) {
+    console.log('GET OCR - Work');
+    const res = await axios.post(`${ENDPOINT}/test/`, {
+        md5: md5Hash,
+        contents: base64data,
+    })
+
+    return res.data;
+}
+
+export async function getOcr(md5Hash, base64data) {
+    console.log('GET OCR');
+    var res;
+    try {
+        res =  await getOcrLazy(md5Hash);
+    } catch (err) {
+        // console.log(err);
+        res = await getOcrWork(md5Hash, base64data);
+    }
+    return res;
 }

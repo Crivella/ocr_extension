14eb6a773690e6bcb9e4971c1ff3e256f9d68671
Crivella <davidecrivella@yahoo.it>
2023-07-19 16:59:46 +0200

    Added license banner to dist files and improved build process

--- webpack.config.js
+++ webpack.config.js
@@ -2,53 +2,62 @@
 
 const path = require('path');
 const webpack = require('webpack');
+const fs = require('fs');
 
-module.exports = {
-  entry: {
-    // Each entry in here would declare a file that needs to be transpiled
-    // and included in the extension source.
-    // For example, you could add a background script like:
-    background: 'background.js',
-    content: 'content.js',
-    popup: 'popup.js',
-    // 'navigate-collection': 'navigate-collection.js',
-  },
-  output: {
-    // This copies each source entry into the extension dist folder named
-    // after its entry config key.
-    path: path.join(__dirname, 'extension', 'dist'),
-    filename: '[name].js',
-  },
-  module: {
-    rules: [
-      {
-        exclude: ['/node_modules/'],
-        test: /\.js$/,
-        use: [
-          // This transpiles all code (except for third party modules) using Babel.
-          {
-            // Babel options are in .babelrc
-            loader: 'babel-loader',
-          },
-        ]
-      }
+module.exports = (env, argv) => {
+  var config = {
+    entry: {
+      // Each entry in here would declare a file that needs to be transpiled
+      // and included in the extension source.
+      // For example, you could add a background script like:
+      background: 'background.js',
+      content: 'content.js',
+      popup: 'popup.js',
+      // 'navigate-collection': 'navigate-collection.js',
+    },
+    output: {
+      // This copies each source entry into the extension dist folder named
+      // after its entry config key.
+      path: path.join(__dirname, 'extension', 'dist'),
+      filename: '[name].js',
+    },
+    module: {
+      rules: [
+        {
+          exclude: ['/node_modules/'],
+          test: /\.js$/,
+          use: [
+            // This transpiles all code (except for third party modules) using Babel.
+            {
+              // Babel options are in .babelrc
+              loader: 'babel-loader',
+            },
+          ]
+        }
+      ]
+    },
+    resolve: {
+      // This allows you to import modules just like you would in a NodeJS app.
+      extensions: ['.js', '.jsx'],
+      modules: [
+        path.join(__dirname, 'src'),
+        'node_modules',
+      ],
+    },
+    plugins: [
+      new webpack.BannerPlugin({
+        banner: fs.readFileSync('License_banner', 'utf8'),
+      })
     ]
-  },
-  resolve: {
-    // This allows you to import modules just like you would in a NodeJS app.
-    extensions: ['.js', '.jsx'],
-    modules: [
-      path.join(__dirname, 'src'),
-      'node_modules',
-    ],
-  },
-  plugins: [
-    // Since some NodeJS modules expect to be running in Node, it is helpful
-    // to set this environment var to avoid reference errors.
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development'),
-    }),
-  ],
-  mode: 'development',
-  devtool: 'source-map',
+  }
+
+  if (argv.mode === 'production') {
+    config.mode = 'production';
+    config.devtool = false;
+  } else {
+    config.mode = 'development';
+    config.devtool = 'source-map';
+  }
+
+  return config;
 };
